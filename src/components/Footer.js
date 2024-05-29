ab79d9d45a26cef1d9812e0354cafbeb9cb3144b
Mirela Mustic <mirela.mustic00@gmail.com>
2024-05-29 15:16:33 +0100

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// src/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 py-4 text-white text-center">
+      <div className="container mx-auto">
+        <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
